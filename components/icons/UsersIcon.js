5e582fbd37c430d2b2e23f4c69df4016c04e096b
enver-isliamov <enverphoto@gmail.com>
2025-07-21 14:01:22 +0300

    Delete components directory

--- components/icons/UsersIcon.js
+++
@@ -1,8 +0,0 @@
-
-import React from 'react';
-
-export const UsersIcon = ({ className }) => (
-  React.createElement('svg', { xmlns: "http://www.w3.org/2000/svg", className: className, fill: "none", viewBox: "0 0 24 24", stroke: "currentColor", strokeWidth: 2 },
-    React.createElement('path', { strokeLinecap: "round", strokeLinejoin: "round", d: "M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M15 21a6 6 0 00-9-5.197m0 0A4 4 0 019 9.354" })
-  )
-);
