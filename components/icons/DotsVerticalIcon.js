45522cf71f1cb85a7a842cbe2b41a0e817bb1e73
enver-isliamov <enverphoto@gmail.com>
2025-07-21 16:44:47 +0300

    Create DotsVerticalIcon.js

---
+++ components/icons/DotsVerticalIcon.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const DotsVerticalIcon = ({ className }) => (
+    React.createElement('svg', { xmlns: "http://www.w3.org/2000/svg", className: className, fill: "none", viewBox: "0 0 24 24", stroke: "currentColor", strokeWidth: 2 },
+        React.createElement('path', { strokeLinecap: "round", strokeLinejoin: "round", d: "M12 5v.01M12 12v.01M12 19v.01" })
+    )
+);
