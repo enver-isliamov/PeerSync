859431b5f8f996cb04dcfc660a660865827af926
enver-isliamov <enverphoto@gmail.com>
2025-07-21 16:35:42 +0300

    Delete components directory

--- components/icons/PencilIcon.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const PencilIcon = ({ className }) => (
-    React.createElement('svg', { xmlns: "http://www.w3.org/2000/svg", className: className, fill: "none", viewBox: "0 0 24 24", stroke: "currentColor", strokeWidth: 2 },
-        React.createElement('path', { strokeLinecap: "round", strokeLinejoin: "round", d: "M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.536L16.732 3.732z" })
-    )
-);
--- components/icons/PlusIcon.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export const PlusIcon = ({ className }) => (
-  React.createElement('svg', { xmlns: "http://www.w3.org/2000/svg", className: className, fill: "none", viewBox: "0 0 24 24", stroke: "currentColor", strokeWidth: 2 },
-    React.createElement('path', { strokeLinecap: "round", strokeLinejoin: "round", d: "M12 4v16m8-8H4" })
-  )
-);
